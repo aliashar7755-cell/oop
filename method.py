c61689e5e7ffdd1b6a227007133d93958cbc80a7
ali ashar v a <aliashar7755@gmail.com>
2026-05-19 09:05:33 +0530

    method.py

--- method.py
+++ method.py
@@ -2,8 +2,8 @@ class person:
     def __init__(self,name):
         self.name = name
 
-        def greet(self):
+    def greet(self):
             print("Hello,my name is "+self.name)
 
-            p1 = person("Emil")
-            p1.greet()
+p1 = person("Emil")
+p1.greet()
